6099a0102e448b2da0f7bf8b9650fdc8a209dd98
Luiz Junior <luizjunior-cte@hotmail.com>
2022-07-04 23:06:54 -0300

    criado estrutura do projeto final

---
+++ src/main/java/br/com/mumia/MumiaAppApplication.java
@@ -0,0 +1,13 @@
+package br.com.mumia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumiaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MumiaAppApplication.class, args);
+	}
+
+}
