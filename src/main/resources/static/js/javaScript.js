1633b62b0f6f9efce1091e9b746ab9e90d36044e
Luiz Junior <luizjunior-cte@hotmail.com>
2022-07-15 22:23:05 -0300

    fuction

---
+++ src/main/resources/static/js/javaScript.js
@@ -0,0 +1,19 @@
+function inserirProduto() {
+	alert('hello');
+}
+
+function updateProduto() {
+	alert('oi');
+}
+
+function deleteProduto() {
+	descricao.value = ""
+}
+
+function buscarProduto() {
+	alert('falo');
+}
+
+function buscarTodosProduto() {
+	alert('valeu');
+}
