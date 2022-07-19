4fbe8998c70a76686e7b359338fa242cf9cae154
Luiz Junior <luizjunior-cte@hotmail.com>
2022-07-19 18:25:46 -0300

    ajustes

--- src/main/resources/static/js/url.js
+++ src/main/resources/static/js/url.js
@@ -1,2 +1,2 @@
-var url = 'http://localhost:8080/'; //url para teste local
-//var url = 'https://mumia-app.herokuapp.com/';   //url para o servidor da heroku 
+//var url = 'http://localhost:8080/'; //url para teste local
+var url = 'https://mumia-app.herokuapp.com/';   //url para o servidor da heroku 
